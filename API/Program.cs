7c44217cef749cc4f76e944803361b1b1c1f3f2d
Drew Sorokin <souljahslm@yahoo.com>
2020-06-06 22:42:21 -0500

    Added CRUD for client app

--- API/Program.cs
+++ API/Program.cs
@@ -6,29 +6,35 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API {
-    public class Program {
-        public static void Main (string[] args) {
-            var host = CreateHostBuilder (args).Build ();
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope ()) {
+            using (var scope = host.Services.CreateScope())
+            {
                 var services = scope.ServiceProvider;
-                try {
-                    var context = services.GetRequiredService<DataContext> ();
-                    context.Database.Migrate ();
-                    Seed.SeedData (context);
-                } catch (Exception ex) {
-                    var logger = services.GetRequiredService<ILogger<Program>> ();
-                    logger.LogError (ex, "an error occurred during migration");
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "an error occurred during migration");
                 }
             }
-            host.Run ();
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
